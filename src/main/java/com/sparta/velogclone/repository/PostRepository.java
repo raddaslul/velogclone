b28fbd367216ae21fd3c7c5a19a5a9e46332748e
raddaslul <raddaslul@naver.com>
2022-02-19 21:37:09 +0900

    게시글 전체 조회 구현

--- src/main/java/com/sparta/velogclone/repository/PostRepository.java
+++ src/main/java/com/sparta/velogclone/repository/PostRepository.java
@@ -3,5 +3,8 @@ package com.sparta.velogclone.repository;
 import com.sparta.velogclone.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllByOOrderByModifiedAtDesc();
 }
